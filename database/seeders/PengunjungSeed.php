2b380c53ee0b4db6c83a3ce9f64599a038cc5187
riorocker75 <gamesourmaktab@gmail.com>
2022-05-11 14:51:23 +0700

    kljkj

--- database/seeders/PengunjungSeed.php
+++ database/seeders/PengunjungSeed.php
@@ -15,30 +15,30 @@ class PengunjungSeed extends Seeder
      */
     public function run()
     {
-         DB::table('pengunjung')->delete();
-        Pengunjung::create([
-          'id' => 1,
-          'nama' => "Sumail",
-          'jenis_kelamin' => "1",
-          'tanggal_lahir' =>  Carbon::now()->format('Y-m-d'),
-          'Alamat' => "Medan Kota",
-          'no_hp' => "085885207894",
-          'tanggal' =>date('Y-m-d'),
-          'lvl' => "1",
-          'status'=> 1
-        ]);
+        //  DB::table('pengunjung')->delete();
+        // Pengunjung::create([
+        //   'id' => 1,
+        //   'nama' => "Sumail",
+        //   'jenis_kelamin' => "1",
+        //   'tanggal_lahir' =>  Carbon::now()->format('Y-m-d'),
+        //   'Alamat' => "Medan Kota",
+        //   'no_hp' => "085885207894",
+        //   'tanggal' =>date('Y-m-d'),
+        //   'lvl' => "1",
+        //   'status'=> 1
+        // ]);
 
  
-        Pengunjung::create([
-          'id' => 2,
-          'nama' => "Taehyung",
-          'jenis_kelamin' => "1",
-          'tanggal_lahir' =>  Carbon::now()->format('Y-m-d'),
-          'Alamat' => "Medan Kota",
-          'no_hp' => "0854654789",
-          'tanggal' =>date('Y-m-d'),
-          'lvl' => "2",
-          'status'=> 1
-        ]);
+        // Pengunjung::create([
+        //   'id' => 2,
+        //   'nama' => "Taehyung",
+        //   'jenis_kelamin' => "1",
+        //   'tanggal_lahir' =>  Carbon::now()->format('Y-m-d'),
+        //   'Alamat' => "Medan Kota",
+        //   'no_hp' => "0854654789",
+        //   'tanggal' =>date('Y-m-d'),
+        //   'lvl' => "2",
+        //   'status'=> 1
+        // ]);
     }
 }
